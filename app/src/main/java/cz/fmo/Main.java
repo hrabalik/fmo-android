e50d44210bc6b0d5ce53968f03f4b05641b564f6
Aleš Hrabalík <tufak064@gmail.com>
2017-01-20 16:20:38 +0100

    Add OpenCV to the project
    SDK must be downloaded separately.

--- app/src/main/java/cz/fmo/Main.java
+++ app/src/main/java/cz/fmo/Main.java
@@ -6,10 +6,19 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
 public class Main extends AppCompatActivity {
+    private static final String TAG = "FMO";
     private static final boolean AUTO_HIDE = true;
     private static final int UI_ANIMATION_DELAY = 300;
 
@@ -49,6 +58,35 @@ public class Main extends AppCompatActivity {
         }
     };
 
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV Manager Connected");
+                    //from now onwards, you can use OpenCV API
+                    Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+                    break;
+                case LoaderCallbackInterface.INIT_FAILED:
+                    Log.i(TAG, "Init Failed");
+                    break;
+                case LoaderCallbackInterface.INSTALL_CANCELED:
+                    Log.i(TAG, "Install Cancelled");
+                    break;
+                case LoaderCallbackInterface.INCOMPATIBLE_MANAGER_VERSION:
+                    Log.i(TAG, "Incompatible Version");
+                    break;
+                case LoaderCallbackInterface.MARKET_ERROR:
+                    Log.i(TAG, "Market Error");
+                    break;
+                default:
+                    Log.i(TAG, "OpenCV Manager Install");
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +112,13 @@ public class Main extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        // loading OpenCV straight from the APK
+        //OpenCVLoader.initDebug();
+
+        // loading OpenCV via OpenCV Manager -- triggers manager installation
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+
         delayedHide(100);
     }
 
