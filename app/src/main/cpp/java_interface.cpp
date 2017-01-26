9660e7e3ca796b4569895c00dc5f0d16023128c5
Aleš Hrabalík <tufak064@gmail.com>
2017-01-26 17:20:53 +0100

    Measure frame times

--- app/src/main/cpp/java_interface.cpp
+++ app/src/main/cpp/java_interface.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <vector>
+#include <algorithm>
 #include <opencv2/core.hpp>
 #include "java_interface.hpp"
 #include "java_classes.hpp"
@@ -30,10 +31,32 @@ jstring Java_cz_fmo_Lib_getHelloString(JNIEnv *env, jclass) {
     return env->NewStringUTF(HELLO_STR);
 }
 
+void timeStats(int64_t deltaNs, const Callback &cb) {
+    static std::vector<int64_t> vec(1000);
+    vec.push_back(deltaNs);
+    if (vec.size() % 100 != 0) return;
+    std::sort(begin(vec), end(vec));
+    auto avg = std::accumulate(begin(vec), end(vec), 0ll) / vec.size();
+    auto iter95 = begin(vec) + ((95 * vec.size()) / 100);
+    auto iter99 = begin(vec) + ((99 * vec.size()) / 100);
+    cb.frameTimings(1e9f / avg, 1e9f / *iter95, 1e9f / *iter99);
+    if (vec.size() >= 18000) vec.clear();
+}
+
 void Java_cz_fmo_Lib_onFrame(JNIEnv *env, jclass, jobject imageObj, jobject cbObj) {
     Image image{env, imageObj};
-    auto size = image.getSize();
-    auto plane = image.getPlane(0);
     Callback cb{env, cbObj};
-    cb.frameTimings(plane.pixelStride, plane.rowStride, image.getTimestamp());
+    int64_t timeNs = image.getTimestamp();
+    static int64_t prevTimeNs = 0;
+
+    if (prevTimeNs != 0) {
+        timeStats(timeNs - prevTimeNs, cb);
+    }
+
+    prevTimeNs = timeNs;
+
+    //auto size = image.getSize();
+    //auto plane = image.getPlane(0);
+    //Callback cb{env, cbObj};
+    //cb.frameTimings(plane.pixelStride, plane.rowStride, image.getTimestamp());
 }
