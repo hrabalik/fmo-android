ac8a717fa84f79a04726dbe97c96a2fbd0d93708
Aleš Hrabalík <tufak064@gmail.com>
2017-01-30 13:53:08 +0100

    Open the camera in OCVRec activity

--- app/src/main/cpp/ocvrec.cpp
+++ app/src/main/cpp/ocvrec.cpp
@@ -1,6 +1,7 @@
 #include "ocvrec.hpp"
 #include <thread>
 #include <atomic>
+#include <opencv2/videoio.hpp>
 
 namespace {
     struct {
@@ -8,9 +9,51 @@ namespace {
         std::atomic<bool> exit;
         Reference<Callback> cbRef;
     } global;
+
+    struct Env {
+        Env(const Env &) = delete;
+
+        Env &operator=(const Env &) = delete;
+
+        Env(const char *threadName) {
+            JavaVMAttachArgs args = {JNI_VERSION_1_6, threadName, nullptr};
+            jint result = global.vm->AttachCurrentThread(&mPtr, nullptr);
+            FMO_ASSERT(result == JNI_OK, "AttachCurrentThread failed");
+        }
+
+        ~Env() { global.vm->DetachCurrentThread(); }
+
+        JNIEnv *get() { return mPtr; }
+
+        JNIEnv &operator->() { return *mPtr; }
+
+    private:
+        JNIEnv *mPtr = nullptr;
+    };
+
+    template<typename Func>
+    struct Finally {
+        Finally(Finally &&rhs) : mFunc(rhs.mFunc) { rhs.valid = false; }
+
+        Finally(Func func) : mFunc(func) { }
+
+        ~Finally() { if (valid) mFunc(); }
+
+        // used to get rid of "unused variable" warnings
+        void dummy() const { }
+
+    private:
+        bool valid = true;
+        Func mFunc;
+    };
+
+    template<typename Func>
+    Finally<Func> finally(Func func) {
+        return {func};
+    }
 }
 
-void loop();
+void ocvRecLoop();
 
 void ocvRecStop() {
     global.exit = true;
@@ -19,22 +62,28 @@ void ocvRecStop() {
 void ocvRecStart(JNIEnv *env, jobject cbObj) {
     global.cbRef = {env, cbObj};
     env->GetJavaVM(&global.vm);
-    std::thread thread{loop};
+    std::thread thread{ocvRecLoop};
     thread.detach();
 }
 
-void loop() {
+void ocvRecLoop() {
+    Env env{"ocvRecLoop"};
     global.exit = false;
-    JNIEnv *env;
-    global.vm->AttachCurrentThread(&env, nullptr);
-    auto cb = global.cbRef.get(env);
+    auto cb = global.cbRef.get(env.get());
+    auto cbCleanup = finally([&env]() { global.cbRef.release(env.get()); });
+    cbCleanup.dummy();
     int frameNum = 0;
+
+    // OpenCV camera init
+    cv::VideoCapture cap;
+    cap.open(0);
+    if (!cap.isOpened()) {
+        cb.cameraError();
+        return;
+    }
 
     while (!global.exit) {
         cb.frameTimings(1, 2, frameNum++);
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-
-    global.cbRef.release(env);
-    global.vm->DetachCurrentThread();
 }
