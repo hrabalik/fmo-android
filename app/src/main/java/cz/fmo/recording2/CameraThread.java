cb4fc00a4a4c420688e922989669cf49e313e6a7
Aleš Hrabalík <tufak064@gmail.com>
2017-02-03 16:47:34 +0100

    Reduce technical debt: CameraThread class

--- app/src/main/java/cz/fmo/recording2/CameraThread.java
+++ app/src/main/java/cz/fmo/recording2/CameraThread.java
@@ -2,6 +2,7 @@ package cz.fmo.recording2;
 
 import android.media.MediaFormat;
 import android.opengl.GLES20;
+import android.support.annotation.Nullable;
 import android.view.Surface;
 
 import cz.fmo.bftp.CameraCapture;
@@ -9,6 +10,12 @@ import cz.fmo.graphics.EGL;
 import cz.fmo.graphics.Renderer;
 import cz.fmo.util.GenericThread;
 
+/**
+ * A separate thread to configure and perform video capture. The captured frames are provided using
+ * two distinct mechanisms. Firstly, one can add surfaces via the addTarget() method; these will
+ * be drawn on using OpenGL. Secondly, one can implement the onCameraFrame() callback, which will
+ * receive the captured frames as a byte array.
+ */
 class CameraThread extends GenericThread<CameraThreadHandler> {
     private final Callback mCb;
     private final java.util.ArrayList<Target> mTargets = new java.util.ArrayList<>();
@@ -16,21 +23,30 @@ class CameraThread extends GenericThread<CameraThreadHandler> {
     private Renderer mRenderer;
     private CameraCapture mCapture;
 
-    CameraThread(Callback cb) {
+    /**
+     * The constructor selects and opens a suitable camera. All methods can be called afterwards.
+     * Precondition: camera permission must be granted, otherwise the camera initialization will
+     * fail.
+     */
+    CameraThread(@Nullable Callback cb) {
         super("CameraThread");
         mCb = cb;
         mCapture = new CameraCapture(mCb);
     }
 
+    /**
+     * Adds a surface that the thread will draw onto using OpenGL. Multiple surfaces can be added.
+     * Call this method before the start() method has been called.
+     */
     void addTarget(Surface surface, int width, int height) {
         mTargets.add(new Target(surface, width, height));
     }
 
-    @Override
-    protected CameraThreadHandler makeHandler() {
-        return new CameraThreadHandler(this);
-    }
-
+    /**
+     * This method is called once the thread is running, but before it starts receiving events.
+     * Since we're running in the correct thread, we are safe to establish an EGL context and
+     * other entities that depend on having it. Finally, we start the capture.
+     */
     @Override
     protected void setup(CameraThreadHandler handler) {
         mEGL = new EGL();
@@ -44,6 +60,10 @@ class CameraThread extends GenericThread<CameraThreadHandler> {
         mCapture.start(mRenderer.getInputTexture());
     }
 
+    /**
+     * This method is called while the thread is running, but after it had stopped receiving events.
+     * Capture is stopped and various cleanup is performed.
+     */
     @Override
     protected void teardown() {
         mCapture.stop();
@@ -69,12 +89,22 @@ class CameraThread extends GenericThread<CameraThreadHandler> {
         }
     }
 
+    /**
+     * This method is triggered by the Renderer.Callback.onFrameAvailable() event. It is called
+     * every frame, as soon as the renderer is ready to draw. Each registered surface is drawn on
+     * using OpenGL.
+     */
     void rendererFrame() {
-        mCb.onCameraRender();
+        if (mCb != null) mCb.onCameraRender();
 
         for (Target target : mTargets) {
             target.render(mRenderer);
         }
+    }
+
+    @Override
+    protected CameraThreadHandler makeHandler() {
+        return new CameraThreadHandler(this);
     }
 
     MediaFormat getMediaFormat() {
@@ -101,6 +131,10 @@ class CameraThread extends GenericThread<CameraThreadHandler> {
         void onCameraRender();
     }
 
+    /**
+     * Internal output surface representation, containing references to the original surface and the
+     * associated EGL.Surface.
+     */
     private static class Target {
         private final Surface mSurface;
         private final int mWidth;
@@ -113,6 +147,10 @@ class CameraThread extends GenericThread<CameraThreadHandler> {
             mHeight = height;
         }
 
+        /**
+         * Create the associated EGL.Surface. For that, the EGL context must be already established
+         * on this thread.
+         */
         void initEGL(EGL egl) {
             mEglSurface = egl.makeSurface(mSurface);
             mEglSurface.makeCurrent();
@@ -125,6 +163,9 @@ class CameraThread extends GenericThread<CameraThreadHandler> {
             }
         }
 
+        /**
+         * Draw the frame onto the target surface using OpenGL.
+         */
         void render(Renderer renderer) {
             mEglSurface.makeCurrent();
             GLES20.glViewport(0, 0, mWidth, mHeight);
