1ca5fe9ffa80a7ca9d898589c5c3f9b6c0ab8777
Aleš Hrabalík <tufak064@gmail.com>
2017-02-17 15:54:12 +0100

    Fix crash due to working with GUI on a non-GUI thread

--- app/src/main/java/cz/fmo/BenchmarkingActivity.java
+++ app/src/main/java/cz/fmo/BenchmarkingActivity.java
@@ -3,11 +3,14 @@ package cz.fmo;
 import android.app.Activity;
 import android.widget.TextView;
 
+import java.lang.ref.WeakReference;
+
 /**
  * Runs the benchmarks and displays the result on screen.
  */
 public final class BenchmarkingActivity extends Activity {
     private final GUI mGUI = new GUI();
+    private final Handler mHandler = new Handler(this);
 
     @Override
     protected void onCreate(android.os.Bundle saved) {
@@ -18,7 +21,7 @@ public final class BenchmarkingActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        Lib.benchmarkingStart(mGUI);
+        Lib.benchmarkingStart(mHandler);
     }
 
     @Override
@@ -27,7 +30,7 @@ public final class BenchmarkingActivity extends Activity {
         Lib.benchmarkingStop();
     }
 
-    private class GUI implements Lib.Callback {
+    private class GUI {
         private TextView mLog;
 
         /**
@@ -38,17 +41,41 @@ public final class BenchmarkingActivity extends Activity {
             mLog = (TextView) findViewById(R.id.benchmarking_log);
         }
 
+        /**
+         * Writes a string to the screen.
+         */
+        void appendToLog(String line) {
+            mLog.append(line);
+        }
+    }
+
+    private static class Handler extends android.os.Handler implements Lib.Callback {
+        static final int LOG = 1;
+        private final WeakReference<BenchmarkingActivity> mActivity;
+
+        Handler(BenchmarkingActivity activity) {
+            mActivity = new WeakReference<>(activity);
+        }
+
         @Override
         public void frameTimings(float q50, float q95, float q99) {
             // ignored
         }
 
-        /**
-         * Writes a string to the screen.
-         */
         @Override
-        public void log(String line) {
-            mLog.append(line);
+        public void log(String string) {
+            sendMessage(obtainMessage(LOG, string));
+        }
+
+        @Override
+        public void handleMessage(android.os.Message msg) {
+            BenchmarkingActivity activity = mActivity.get();
+            if (activity == null) return;
+
+            switch (msg.what) {
+                case LOG:
+                    activity.mGUI.appendToLog((String) msg.obj);
+            }
         }
     }
 }
